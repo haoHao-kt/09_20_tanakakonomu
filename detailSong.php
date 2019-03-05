bdad399db6e80487eb5783c04429a75279a89d72
haoHao-kt <konomu.tanaka.sk@gmail.com>
2019-03-05 19:54:57 +0900

    initial commit

---
+++ detailSong.php
@@ -0,0 +1,33 @@
+<?php
+// セッションのスタート
+session_start();
+include('functions.php');
+$pdo = createPDO();
+
+// $comp_id = $_GET['comp_id'];
+// $song_id = $_GET['song_id'];
+
+$comp_id = '1000';
+$song_id = '1';
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>曲詳細</title>
+</head>
+<body>
+
+<script>
+
+
+</script>    
+</body>
+</html>
